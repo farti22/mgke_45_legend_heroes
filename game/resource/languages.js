4b4bf8a015cd51913c26818a19f9d4fd594d8f17
BlackDebil <kononovih01@gmail.com>
2022-04-07 11:57:20 +0300

    develop: Add localization code

---
+++ game/resource/languages.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Language = void 0;
+var Language;
+(function (Language) {
+    Language["None"] = "none";
+    Language["English"] = "english";
+    Language["Russian"] = "russian";
+    Language["Schinese"] = "schinese";
+})(Language = exports.Language || (exports.Language = {}));
---
+++ game/resource/languages.ts
@@ -0,0 +1,7 @@
+export enum Language
+{
+    None = 'none',
+    English = 'english',
+    Russian = `russian`,    
+    Schinese = `schinese`,
+}
